0ae0b9f7b5df3d88894aaef7b6fbea22e163223a
teo-sl <teodoro.sullazzo@gmail.com>
2022-11-16 10:13:35 +0100

    working heap_sort, need to fix fix-heap-down

--- AI/exercises/lisp/heap_sort.lisp
+++ AI/exercises/lisp/heap_sort.lisp
@@ -1,7 +1,7 @@
 (defun binary-list (n)
   (cond ((= n 3) (list 1))
         ((= n 2) (list 0))
-        ((= n 1) (list 1)) 
+        ((= n 1) ()) 
         (t 
              (append 
                   (binary-list (truncate n 2)) 
@@ -12,7 +12,7 @@
 )
 
 
-(defun insert-heap-0(L X BS)
+(defun insert-heap-0 (L X BS)
     
     (cond
         ((not BS) 
@@ -84,6 +84,9 @@
 
 
 
+
+
+
 (defun feed-heap (L VS H HS)
     (cond
         ((= VS 0)
@@ -100,9 +103,162 @@
     )
 )
 
-(defun heap_sort (L)
-    (feed-heap L (length L) () 0 )
+(defun get-nth (H BL)
+    (cond
+        ((not BL)
+            (car H)
+        )
+        ((= (car BL) 1)
+            (get-nth (caddr H) (cdr BL))
+        ) 
+        (t
+            (get-nth (cadr H) (cdr BL))
+        )
+    )
 )
+
+(defun delete-nth (H BL)
+    (cond
+        ((not BL)
+            '()
+        )
+        ((= (car BL) 1)
+            (list
+                (car H)
+                (cadr H)
+                (delete-nth (caddr H) (cdr BL))
+            )
+            
+        ) 
+        (t
+            (list
+                (car H)
+                (delete-nth (cadr H) (cdr BL))
+                (caddr H)
+            )
+        )
+    )
+
+)
+(defun set-root (H V)
+
+    (list
+        V
+        (cadr H)
+        (caddr H)
+    )
+)
+(defun min-son (H) 
+    (cond
+        ((and (not (cadr H)) (not (caddr H)))
+            '()
+        )
+        ((not (cadr H))
+            (list  1 (caaddr H))
+        )
+        ((not (caddr H))
+            (list 0 (caadr H))
+        )
+        ((< (caadr H) (caaddr H))
+            (list  0 (caadr H))
+        )
+        (t
+            (list 1 (caaddr H))
+        )
+    )
+    
+)
+
+(defun fix-heap-down (H son_value)
+    (setq min_son (min-son H))
+    (cond
+        ((not H)
+            '()
+        )
+        ((not min_son)
+
+            (list son_value '() '())
+        )
+        ((< (car H) (cadr min_son)) 
+            (list
+                son_value
+                (cadr H)
+                (caddr H)
+            )
+        )
+        ( (= (car min_son ) 0) 
+            
+            (setq son_son_value (max (car H) son_value))
+            (setq my_value (min (caadr H) son_value))
+
+            (list
+                my_value
+                (fix-heap-down (cadr H) son_son_value )
+                (caddr H)
+            )
+        )
+        ((= (car min_son ) 1)
+            
+            (setq son_son_value (max (car H) son_value))
+            (setq my_value (min (caaddr H) (car H)))
+            (list
+                my_value
+                (cadr H)
+                (fix-heap-down (caddr H)  son_son_value ) 
+            )
+        )
+    )
+
+)
+
+
+(defun delete-root (H SH)
+    (setq BL (binary-list SH))
+    (setq new_root (get-nth H BL))
+    (print (set-root (delete-nth H BL) new_root))
+    (print new_root)
+    (fix-heap-down 
+        (set-root (delete-nth H BL) new_root)
+        new_root
+    )
+    
+)
+
+(defun empty-heap (H SH)
+    
+    (cond
+        ((= SH 1)
+            (list (car H))
+        )
+        ((> SH 0)
+            (append
+                (list (car H))
+                (empty-heap (delete-root H SH) (- SH 1))
+            )
+
+            
+        )
+
+    )
+    
+
+)
+
+(defun heap_sort (L)
+    (setq full_heap (feed-heap L (length L) () 0 ))
+    (print full_heap)
+    (empty-heap full_heap (length L))
+)
+
+
+(setq h (heap_sort '(5 1 3 9 4 12 78 32 0 21 43)))
+;(setq h '( 4 ( 9 () () ) (5 () ())))
+
+
+;(print (delete-root h 3))
+
+;(delete-root h 3)
+
 
 
 
